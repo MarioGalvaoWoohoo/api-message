81698540252e4452630591531abd9c059b987139
MarioGalvaoWoohoo <65731257+MarioGalvaoWoohoo@users.noreply.github.com>
2023-04-22 18:10:00 -0300

    Rodando script sh

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo $USER_PASS | sudo -S docker exec -it app-message /bin/bash -c "cd projects/api-message && sudo -u root composer install"
+ls -la
+echo $USER_PASS | sudo -S docker exec -T app-message /bin/bash -c "cd projects/api-message && sudo -u root composer install"
